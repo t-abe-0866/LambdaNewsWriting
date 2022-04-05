d2f58bbc481ab0cf6f0852234dadf28180b1f520
t-abe0866 <t.abe@nippo-co.com>
2022-04-05 11:31:49 +0900

    rds

--- lambda-function.py
+++ lambda-function.py
@@ -1,8 +1,20 @@
 import json
+import boto3
+import psycopg2
+import os
+import time
 
 def lambda_handler(event, context):
-    # TODO implement
+    # rds保存処理
+    try:
+        conn = psycopg2.connect(host=os.environ['RDS_ENDPOINT'], database=os.environ['RDS_DBNAME'], user=os.environ['RDS_USR'],password=os.environ['RDS_PASS'], connect_timeout=3)
+        cur = conn.cursor()
+        cur.execute(INSERT INTO News VALUES (2, 'b', '2021/06/08 12:00:01', 'c'))
+        results = conn.commit()
+        print(results)
+    except psycopg2.OperationalError as e:
+        print(str(e))
     return {
         'statusCode': 200,
-        'body': json.dumps('aiueo!')
+        'body': json.dumps('ok!')
     }
