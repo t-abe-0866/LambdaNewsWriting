935e4916738ab3e9d3218a8f45f1119840c03cfc
t-abe0866 <t.abe@nippo-co.com>
2022-04-05 13:18:34 +0900

    Scraping

--- lambda_function.py
+++ lambda_function.py
@@ -1,15 +1,18 @@
 import json
-import boto3
 import psycopg2
 import os
-import time
+import feedparser
 
 def lambda_handler(event, context):
     # rds保存処理
     try:
         conn = psycopg2.connect(host=os.environ['RDS_ENDPOINT'], database=os.environ['RDS_DBNAME'], user=os.environ['RDS_USR'],password=os.environ['RDS_PASS'], connect_timeout=3)
         cur = conn.cursor()
-        cur.execute('INSERT INTO news (news_id,title,date,link) VALUES (%s,%s,%s,%s)',(2,"b","2021/06/08 12:00:01","c"))
+        
+        #スクレイピング
+        d = feedparser.parse('https://www.lifehacker.jp/feed')
+        for entry in d.entries:
+            cur.execute('INSERT INTO news (title,date,link) VALUES (%s,%s,%s)',(entry.title,entry.lastBuildDate,entry.link))
         results = conn.commit()
         print(results)
     except psycopg2.OperationalError as e:
